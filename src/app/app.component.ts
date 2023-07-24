71a19102629794f1bf0a606d33f673daaa09de0e
Eduardo Magnus <eduj.magnus@gmail.com>
2023-07-23 23:17:30 -0300

    First demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { NewWord } from './NewWord';
+
+interface DisabledLetters {
+  [index: string]: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  
   title = 'anagrams-fe';
+
+  constructor(private http: HttpClient) { }
+
+  word: string[];
+  
+  scrambledWord: string[];
+
+  disabledLetters: DisabledLetters = {}
+
+  guess: string[];
+
+  difficulties = ['Easy', 'Medium', 'Intermediate', 'Challenging', 'Hard', 'Expert', 'Master']
+
+  difficulty = 'Easy';
+  
+  languages = ['Portuguese', 'English', 'German', 'French']
+
+  language = 'Portuguese'
+
+  isGuessCorrect: boolean = false;
+
+  readonly ROOT_URL = 'http://127.0.0.1:5000'
+  
+  getNewWord() {
+    this.scrambledWord = new Array;
+    const response = this.http.post<NewWord>(this.ROOT_URL + '/getword', { "difficulty": this.difficulty, "language": this.language })
+    response.subscribe((current) => {
+      this.word = Array.from(current.word)
+      this.scrambledWord = Array.from(current.scrambledWord)
+      this.guess = Array(this.scrambledWord.length).fill('')
+      
+      for (let index = 0; index < this.scrambledWord.length; index++) {
+        this.disabledLetters[index] = false;
+      }
+    })
+  }
+
+  addLetterToGuess(letter:string) {
+    for (let i = 0; i < this.guess.length; i++) {
+      if (this.guess[i] == '') {
+        this.guess[i] = letter;
+        break;
+      }
+      
+    }
+    console.log(this.isGuessCorrect)
+    this.isGuessRight()
+    console.log(this.isGuessCorrect)
+  }
+
+  disableButton(i:number) {
+    this.disabledLetters[i] = true
+  }
+
+  enableButton(letter:string) {
+    for (let index = 0; index < this.scrambledWord.length; index++) {
+      if (this.scrambledWord[index] === letter && this.disabledLetters[index] === true) {
+        this.disabledLetters[index] = false;
+      }
+      
+    }
+  }
+
+  isGuessRight() {
+    console.log('word: ',this.word.toString())
+    console.log('guess: ', this.guess.toString())
+    if (this.word.toString() == this.guess.toString()) {
+      this.isGuessCorrect = true;
+    }
+  }
+
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LetterOptionComponent } from './letter-option/letter-option.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LetterSelectedComponent } from './letter-selected/letter-selected.component'
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatChipsModule } from '@angular/material/chips';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetterOptionComponent,
+    LetterSelectedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
